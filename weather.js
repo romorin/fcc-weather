93c7ba009e9c7926a0060b5f0b2e56578a09f93b
romorin <romorin@zoho.com>
2016-06-05 00:37:44 -0400

    completed OOisation - makes tings nicer

--- weather.js
+++ weather.js
@@ -29,6 +29,10 @@ var images = {};
 var LABEL_INPUT = "Go!";
 var LABEL_SHOW = "Change!";
 
+/*********************************************************
+	Json
+*********************************************************/
+
 /////////////////////////////////////////////////////////
 // data for testing purpose
 
@@ -70,13 +74,39 @@ function getWeatherJson(parser, loc) {
 	Weather dom section
 *********************************************************/
 
+
+function getNestedProp(json, path) {
+	return getNestedPropImpl(json, path, 0);
+}
+
+// path can be array index or object key, since arrays are objects
+function getNestedPropImpl(json, path, depth) {
+	// end check
+	if (path === undefined) {
+		return "";
+	}
+
+	if(depth >= path.length) {
+		return json;
+	}
+	// error check
+	if (!json.hasOwnProperty(path[depth])) {
+		return "";
+	}
+	// dig deeper
+	return getNestedPropImpl(json[path[depth]], path, depth+1);
+}
+
 /////////////////////////////////////////////////////////
 // base elem
 
 function WeatherElem() {
 }
 
-WeatherElem.prototype.writeHtml = function () {
+WeatherElem.prototype.onJsonUpdate = function () {
+};
+
+WeatherElem.prototype.onDegreeChange = function () {
 };
 
 /////////////////////////////////////////////////////////
@@ -93,7 +123,7 @@ var HtmlWeatherElem = (function () {
 	HtmlWeatherElem.prototype = Object.create(WeatherElem.prototype);
 	HtmlWeatherElem.prototype.constructor = HtmlWeatherElem;
 
-	HtmlWeatherElem.prototype.writeHtml = function (json) {
+	HtmlWeatherElem.prototype.onJsonUpdate = function (json) {
 		jQuery(this._id).html(getNestedProp(json, this._path));
 	};
 
@@ -114,7 +144,7 @@ var DateWeatherElem = (function () {
 	DateWeatherElem.prototype = Object.create(WeatherElem.prototype);
 	DateWeatherElem.prototype.constructor = DateWeatherElem;
 
-	DateWeatherElem.prototype.writeHtml = function (json) {
+	DateWeatherElem.prototype.onJsonUpdate = function (json) {
 		var date = new Date(getNestedProp(json, this._path) * 1000);
 		jQuery(this._id).html(date.toTimeString().split(' ', 1));
 	};
@@ -159,7 +189,8 @@ var TempWeatherElem = (function () {
 	TempWeatherElem.prototype = Object.create(WeatherElem.prototype);
 	TempWeatherElem.prototype.constructor = TempWeatherElem;
 
-	TempWeatherElem.prototype.writeHtml = function (json) {
+	TempWeatherElem.prototype.onDegreeChange =
+	TempWeatherElem.prototype.onJsonUpdate = function (json) {
 		var contents = getNestedProp(json, this._path);
 		var value = {'k':contents, 'c':getCelsius(contents), 'f':getFahrenheit(contents)};
 		//tempValues[this._id] = value;
@@ -187,7 +218,7 @@ var ImgWeatherElem = (function() {
 	ImgWeatherElem.prototype = Object.create(WeatherElem.prototype);
 	ImgWeatherElem.prototype.constructor = ImgWeatherElem;
 
-	ImgWeatherElem.prototype.writeHtml = function (json) {
+	ImgWeatherElem.prototype.onJsonUpdate = function (json) {
 		var contents = getNestedProp(json, this._path);
 		jQuery(this._id).prop("src", [this._pre, contents, this._post].join(""));
 	};
@@ -240,7 +271,7 @@ var BackgroundWeatherElem = (function() {
 	BackgroundWeatherElem.prototype = Object.create(WeatherElem.prototype);
 	BackgroundWeatherElem.prototype.constructor = BackgroundWeatherElem;
 
-	BackgroundWeatherElem.prototype.writeHtml = function (json) {
+	BackgroundWeatherElem.prototype.onJsonUpdate = function (json) {
 		var icon = getNestedProp(json, this._path);
 
 		// update background
@@ -255,113 +286,113 @@ var BackgroundWeatherElem = (function() {
 })();
 
 /////////////////////////////////////////////////////////
-// weather elems
+// weather json handler
 
-function WeatherElems() {
-	this.elems = [];
-
-	this.elems.push(new TempWeatherElem('#temperature', ["main", "temp"]));
-	this.elems.push(new HtmlWeatherElem('#humidity', ["main", "humidity"]));
-	this.elems.push(new TempWeatherElem('#minimum', ["main", "temp_min"]));
-	this.elems.push(new TempWeatherElem('#maximum', ["main", "temp_max"]));
-	this.elems.push(new HtmlWeatherElem('#wind-speed', ["wind", "speed"]));
-	this.elems.push(new HtmlWeatherElem('#wind-deg', ["wind", "deg"]));
-	this.elems.push(new HtmlWeatherElem('#coverage', ["clouds", "all"]));
-	this.elems.push(new HtmlWeatherElem('#city', ["name"]));
-	this.elems.push(new HtmlWeatherElem('#country', ["sys", "country"]));
-	this.elems.push(new DateWeatherElem('#sunrise', ["sys", "sunrise"]));
-	this.elems.push(new DateWeatherElem('#sunset', ["sys", "sunset"]));
-	this.elems.push(new HtmlWeatherElem('#weather-name', ["weather", 0, "main"]));
-	this.elems.push(new HtmlWeatherElem('#weather-desc', ["weather", 0, "description"]));
-	this.elems.push(new ImgWeatherElem("#weather-icon", ["weather", 0, "icon"],
-													"http://openweathermap.org/img/w/", ".png"));
-	this.elems.push(new BackgroundWeatherElem("#background", ["weather", 0, "icon"]));
-}
-
-WeatherElems.prototype.writeHtml = function (json) {
-	var length = this.elems.length;
-	for (var i = 0; i < length; i++) {
-		this.elems[i].writeHtml(json);
+var WeatherJsonHandler = (function() {
+	function initElems(self) {	
+		self._elems.push(new TempWeatherElem('#temperature', ["main", "temp"]));
+		self._elems.push(new HtmlWeatherElem('#humidity', ["main", "humidity"]));
+		self._elems.push(new TempWeatherElem('#minimum', ["main", "temp_min"]));
+		self._elems.push(new TempWeatherElem('#maximum', ["main", "temp_max"]));
+		self._elems.push(new HtmlWeatherElem('#wind-speed', ["wind", "speed"]));
+		self._elems.push(new HtmlWeatherElem('#wind-deg', ["wind", "deg"]));
+		self._elems.push(new HtmlWeatherElem('#coverage', ["clouds", "all"]));
+		self._elems.push(new HtmlWeatherElem('#city', ["name"]));
+		self._elems.push(new HtmlWeatherElem('#country', ["sys", "country"]));
+		self._elems.push(new DateWeatherElem('#sunrise', ["sys", "sunrise"]));
+		self._elems.push(new DateWeatherElem('#sunset', ["sys", "sunset"]));
+		self._elems.push(new HtmlWeatherElem('#weather-name', ["weather", 0, "main"]));
+		self._elems.push(new HtmlWeatherElem('#weather-desc', ["weather", 0, "description"]));
+		self._elems.push(new ImgWeatherElem("#weather-icon", ["weather", 0, "icon"],
+														"http://openweathermap.org/img/w/", ".png"));
+		self._elems.push(new BackgroundWeatherElem("#background", ["weather", 0, "icon"]));
 	}
-};
+	
+	function WeatherJsonHandler() {
+		this._elems = [];
+		this._json = {};
+		
+		initElems(this);
+	}
 
-WeatherElems.prototype.writeTempsHtml = function (json) {
-	//todo
-};
-
-var WEATHER_ELEMS = new WeatherElems();
+	WeatherJsonHandler.prototype.onJsonUpdate = function (json) {
+		this._json = json;
+		
+		var length = this._elems.length;
+		for (var i = 0; i < length; i++) {
+			this._elems[i].onJsonUpdate(this._json);
+		}
+	};
+	
+	WeatherJsonHandler.prototype.onDegreeChange = function () {
+		var length = this._elems.length;
+		for (var i = 0; i < length; i++) {
+			this._elems[i].onDegreeChange(this._json);
+		}
+	};
+	return WeatherJsonHandler;
+})();
 
 /*********************************************************
-	Top section
+	Page handler
 *********************************************************/
 
-function getNestedProp(json, path) {
-	return getNestedPropImpl(json, path, 0);
-}
-
-// path can be array index or object key, since arrays are objects
-function getNestedPropImpl(json, path, depth) {
-	// end check
-	if (path === undefined) {
-		return "";
+var WeatherPageHandler = (function () {	
+	function processWeatherJson(self, json) {
+		if (json.cod === 200) {
+			jQuery("#error-block").hide();
+			self._jsonHandler.onJsonUpdate(json);
+			jQuery("#weather-block").show();
+			jQuery("#background").show();
+			
+			jQuery("#input-location").hide();
+			jQuery("#label-location").show();
+			jQuery("#go-button").html(LABEL_SHOW);
+		} else {
+			jQuery("#weather-block").hide();
+			jQuery("#background").hide();
+			jQuery('#status').html(json.message);
+			jQuery("#error-block").show();
+		}
+	}
+	
+	function getParserPtr(self) {
+		return function (json) {
+			processWeatherJson(self, json);
+		};
+	}
+	
+	function WeatherPageHandler() {
+		this._jsonHandler = new WeatherJsonHandler();
 	}
 
-	if(depth >= path.length) {
-		return json;
-	}
-	// error check
-	if (!json.hasOwnProperty(path[depth])) {
-		return "";
-	}
-	// dig deeper
-	return getNestedPropImpl(json[path[depth]], path, depth+1);
-}
+	// main function to display the weather
+	WeatherPageHandler.prototype.onActionClick = function() {
+		var inputElem = jQuery("#input-location");
+		if (inputElem.is(":visible")) {
+			var loc = encodeURIComponent(inputElem.val());
+			getJsonFct(getParserPtr(this), loc);
+		} else {
+			jQuery("#input-location").show();
+			jQuery("#label-location").hide();
+			jQuery("#go-button").html(LABEL_INPUT);
+		}
+	};
+	
+	WeatherPageHandler.prototype.onDegreeChange = function() {
+		this._jsonHandler.onDegreeChange();
+	};
 
-function processWeatherJson(json) {
-	if (json.cod === 200) {
-		jQuery("#error-block").hide();
-		WEATHER_ELEMS.writeHtml(json);
-		setLocationState(false);
-		jQuery("#weather-block").show();
-		jQuery("#background").show();
-	} else {
-		jQuery("#weather-block").hide();
-		jQuery("#background").hide();
-		jQuery('#status').html(json.message);
-		jQuery("#error-block").show();
-	}
-}
+	return WeatherPageHandler;
+})();
 
-function setLocationState(toInput) {
-	var inputElem = jQuery("#input-location");
-	var labelElem = jQuery("#label-location");
-	var actionButton = jQuery("#go-button");
+var PAGE_HANDLER = new WeatherPageHandler();
 
-	if (toInput === true) {
-		inputElem.show();
-		labelElem.hide();
-		actionButton.html(LABEL_INPUT);
-	} else {
-		inputElem.hide();
-		labelElem.show();
-		actionButton.html(LABEL_SHOW);
-	}
-}
-
-// main function to display the weather
-function onActionClick() {
-	var inputElem = jQuery("#input-location");
-	if (inputElem.is(":visible")) {
-		var loc = encodeURIComponent(inputElem.val());
-		getJsonFct(processWeatherJson, loc);
-	} else {
-		setLocationState(true);
-	}
-}
-
-function onDegreeChange() {
-	WEATHER_ELEMS.writeTempsHtml();
-}
+// to be done when the page is ready
+jQuery(document).ready(function() {
+	jQuery("#go-button").on("click", PAGE_HANDLER.onActionClick.bind(PAGE_HANDLER));
+	jQuery("input[name=degree]:radio").change(PAGE_HANDLER.onDegreeChange.bind(PAGE_HANDLER));
+});
 
 function preload() {
 	for (var url in PIC_URL) {
@@ -370,13 +401,7 @@ function preload() {
 	}
 }
 
-// to be done when the page is ready
-jQuery(document).ready(function() {
-	jQuery("#go-button").on("click", onActionClick);
-	jQuery("input[name=degree]:radio").change(onDegreeChange);
-});
-
 /* Preload code */
-$(window).load( function(){
+jQuery(window).load( function(){
 	preload();
 });
